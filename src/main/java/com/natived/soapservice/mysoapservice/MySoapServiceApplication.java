cc3e0ed35092d650c734329a71759d683ec8f1c8
Edward Natividad <mtdenatividad@gmaill.com>
2020-05-17 10:32:35 -0400

    initial revision

---
+++ src/main/java/com/natived/soapservice/mysoapservice/MySoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.natived.soapservice.mysoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySoapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySoapServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/natived/soapservice/mysoapservice/MySoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natived.soapservice.mysoapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySoapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
